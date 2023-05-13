7b93fa54c21f093e426904208609898c89876cb8
yuuliyz <130220904+yuuliyz@users.noreply.github.com>
2023-05-13 16:53:49 +0500

    v 0.0.1

---
+++ main.py
@@ -0,0 +1,16 @@
+from pygame import *
+init()
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import *
+
+app = QApplication([])
+scene = QWidget()
+scene.resize(900, 700)
+
+button = QPushButton('новая игра')
+button2 = QPushButton('')
+button3 = QPushButton('')
+
+
+scene.show
+app.exec_
