3c5a6dbe76e9deff5ecbc245c0805549956d8dd3
yuuliyz <130220904+yuuliyz@users.noreply.github.com>
2023-05-20 15:56:19 +0500

    v 0.0.2

--- main.py
+++ main.py
@@ -3,14 +3,32 @@ init()
 from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import *
 
+game = True
+
+
+
+
 app = QApplication([])
 scene = QWidget()
 scene.resize(900, 700)
 
-button = QPushButton('новая игра')
-button2 = QPushButton('')
-button3 = QPushButton('')
+button1 = QPushButton('начать игру')
+button2 = QPushButton('настройки')
+button3 = QPushButton('ааааа')
 
+main_line = QVBoxLayout()
+main_line.addWidget(button1, alignment= Qt.AlignCenter)
+main_line.addWidget(button2, alignment= Qt.AlignCenter)
+main_line.addWidget(button3, alignment= Qt.AlignCenter)
+
+def game():
+    if button1.clicked.connect() == True:
+        while game:
+            
+
+
+
+scene.setLayout(main_line)
 
 scene.show
 app.exec_
